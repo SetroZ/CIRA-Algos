92a8c29279b1e404a8dc995394a50b844240ccac
SetroZ <yousfsbry66@gmail.com>
2024-11-21 07:38:55 +0800

    removed cd src from shell

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd src/code
 g++ -g -Ofast  -o main main.cpp writer.cpp brute.cpp -I/usr/local/include -L/usr/local/lib -lCCfits -lcfitsio 
--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # valgrind  --leak-check=full --show-leak-kinds=all 
 ./build.sh
-cd src/code
-
 ./main  $1 $2
 # gdb -ex run --args ./main "$@" && echo
 
